5087561c188b1b16ed561e8e92fab5dc42df614e
O. HADALIN <dyblikat777@gmail.com>
2024-10-26 16:41:49 +0300

    task-4

--- src/main.js
+++ src/main.js
@@ -112,21 +112,26 @@ const btnInput = document.querySelector('.login-form button');
 // Якщо значення парне, вирішуй проміс та повертай "even" через 1 секунду.
 // Якщо значення не парне, вирішуй проміс та повертай "odd" через 2 секунди.
 
-const promptValue = +prompt('Enter something');
-function checkValue (promptValue) {
-    return new Promise ((resolve, reject) => {
-if(isNaN(promptValue)) {
-reject('error')
-}
-if(promptValue % 2 === 0) {
-  setTimeout(() => resolve('even'), 1000)
-} 
-if(promptValue % 2 !== 0) {
-    setTimeout(() => resolve('odd'), 2000)
-}
-    })
-}
+// const promptValue = +prompt('Enter something');
+// function checkValue (promptValue) {
+//     return new Promise ((resolve, reject) => {
+// if(isNaN(promptValue)) {
+// reject('error')
+// }
+// if(promptValue % 2 === 0) {
+//   setTimeout(() => resolve('even'), 1000)
+// }
+// if(promptValue % 2 !== 0) {
+//     setTimeout(() => resolve('odd'), 2000)
+// }
+//     })
+// }
 
-checkValue(promptValue) 
-.then(value => console.log(value))
-.catch(error => console.log(error))
+// checkValue(promptValue)
+// .then(value => console.log(value))
+// .catch(error => console.log(error))
+
+// Додай відображення дати і часу в реальному часі
+const timeCurrent = document.querySelector(".date span");
+timeCurrent.textContent = new Date().toLocaleString("uk-UA");
+setInterval(() => timeCurrent.textContent = new Date().toLocaleString("uk-UA"), 1000);
