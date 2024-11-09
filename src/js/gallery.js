be1458531109a5a28788d0a9671d55417f611d35
Yurii <oniks9821@gmail.com>
2024-11-09 17:04:36 +0200

    1704

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -17,6 +17,7 @@ const container = document.getElementById('pagination');
 const pagination = new Pagination(container, options);
 const page = pagination.getCurrentPage();
 const form = document.querySelector('.search-form');
+const loader = document.querySelector('.loader');
 
 pagination.on('afterMove', getPopular);
 
@@ -27,46 +28,66 @@ api
   .then(res => {
     gallery.innerHTML = renderMarkup(res.results);
     pagination.reset(res.total);
+    container.classList.remove('hiden');
   })
-  .catch(console.log);
+  .catch(error => {
+    console.log(error);
+    iziToast.error({
+      message: 'Error',
+    });
+  })
+  .finally(() => loader.classList.add('hiden'));
 
 form.addEventListener('submit', event => {
   event.preventDefault();
   const inputValue = event.target.elements.query.value.trim();
   if (inputValue === '') {
     iziToast.info({
-  message: 'Sorry, enter that you want to find'
-})
+      message: 'Sorry, enter that you want to find',
+    });
     return;
   }
   pagination.off('afterMove', getPopular);
-  pagination.off ('afterMove', getByQuery)
+  pagination.off('afterMove', getByQuery);
   api.query = inputValue;
+
+  loader.classList.remove('hiden');
+
   api
     .fetchPhotosByQuery(page)
     .then(res => {
       if (res.results.length === 0) {
-iziToast.info({
-  message: 'Sorry, enter other query'
-})
-return;
+        iziToast.info({
+          message: 'Sorry, enter other query',
+        });
+        return;
+      }
+      if (res.total > 12) {
+        container.classList.remove('hiden');
+      } else {
+        container.classList.add('hiden');
       }
       gallery.innerHTML = renderMarkup(res.results);
       pagination.reset(res.total);
-      
     })
-    .catch(error => console.log(error));
-pagination.on ('afterMove', getByQuery)
+    .catch(error => {
+      console.log(error);
+      iziToast.error({
+        message: 'Error',
+      });
+    })
+    .finally(() => loader.classList.add('hiden'));
+  pagination.on('afterMove', getByQuery);
 });
 
-function getPopular (event) {
+function getPopular(event) {
   const currentPage = event.page;
   api.fetchPopularPhotos(currentPage).then(res => {
     gallery.innerHTML = renderMarkup(res.results);
   });
 }
 
-function getByQuery (event) {
+function getByQuery(event) {
   const currentPage = event.page;
   api.fetchPhotosByQuery(currentPage).then(res => {
     gallery.innerHTML = renderMarkup(res.results);
