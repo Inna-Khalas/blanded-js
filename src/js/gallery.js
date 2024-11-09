caf6f2a562027b66eadf2e36b790644b85a98cde
Olha <oyashnaya88@gmail.com>
2024-11-09 16:27:18 +0200

    last changes

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,5 +1,7 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 import UnsplashAPI from './UnsplashAPI';
 import renderMarkup from './renderFn';
@@ -16,13 +18,7 @@ const pagination = new Pagination(container, options);
 const page = pagination.getCurrentPage();
 const form = document.querySelector('.search-form');
 
-pagination.on('afterMove', event => {
-  const currentPage = event.page;
-
-  api.fetchPopularPhotos(currentPage).then(res => {
-    gallery.innerHTML = renderMarkup(res.results);
-  });
-});
+pagination.on('afterMove', getPopular);
 
 const api = new UnsplashAPI();
 
@@ -38,14 +34,41 @@ form.addEventListener('submit', event => {
   event.preventDefault();
   const inputValue = event.target.elements.query.value.trim();
   if (inputValue === '') {
+    iziToast.info({
+  message: 'Sorry, enter that you want to find'
+})
     return;
   }
+  pagination.off('afterMove', getPopular);
+  pagination.off ('afterMove', getByQuery)
   api.query = inputValue;
   api
     .fetchPhotosByQuery(page)
     .then(res => {
+      if (res.results.length === 0) {
+iziToast.info({
+  message: 'Sorry, enter other query'
+})
+return;
+      }
       gallery.innerHTML = renderMarkup(res.results);
       pagination.reset(res.total);
+      
     })
     .catch(error => console.log(error));
+pagination.on ('afterMove', getByQuery)
 });
+
+function getPopular (event) {
+  const currentPage = event.page;
+  api.fetchPopularPhotos(currentPage).then(res => {
+    gallery.innerHTML = renderMarkup(res.results);
+  });
+}
+
+function getByQuery (event) {
+  const currentPage = event.page;
+  api.fetchPhotosByQuery(currentPage).then(res => {
+    gallery.innerHTML = renderMarkup(res.results);
+  });
+}
