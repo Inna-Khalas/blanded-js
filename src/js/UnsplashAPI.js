e5e1bba203c91b3be31d94b2ea23fd89395bddb2
oksana-rybalka <rybalka-oksana@outlook.com>
2024-11-09 15:48:28 +0200

    serch

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 export default class UnsplashAPI {
   #API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58';
   #BASE_URL = 'https://api.unsplash.com/search/photos';
+  #query = '';
 
   async fetchPopularPhotos(page = 1) {
     const response = await axios(this.#BASE_URL, {
@@ -17,5 +18,23 @@ export default class UnsplashAPI {
     });
 
     return response.data;
+  }
+  async fetchPhotosByQuery(page = 1) {
+    const response = await axios(this.#BASE_URL, {
+      params: {
+        query: this.#query,
+        page,
+        per_page: 12,
+        orientation: 'portrait',
+      },
+      headers: {
+        Authorization: `Client-ID ${this.#API_KEY}`,
+      },
+    });
+
+    return response.data;
+  }
+  set query(newQuery) {
+    this.#query = newQuery;
   }
 }
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -14,6 +14,7 @@ const gallery = document.querySelector('.gallery');
 const container = document.getElementById('pagination');
 const pagination = new Pagination(container, options);
 const page = pagination.getCurrentPage();
+const form = document.querySelector('.search-form');
 
 pagination.on('afterMove', event => {
   const currentPage = event.page;
@@ -32,3 +33,19 @@ api
     pagination.reset(res.total);
   })
   .catch(console.log);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const inputValue = event.target.elements.query.value.trim();
+  if (inputValue === '') {
+    return;
+  }
+  api.query = inputValue;
+  api
+    .fetchPhotosByQuery(page)
+    .then(res => {
+      gallery.innerHTML = renderMarkup(res.results);
+      pagination.reset(res.total);
+    })
+    .catch(error => console.log(error));
+});
